742c8e3a4f9da806acc032b62079079c2149def1
charlieoduk <odukjr@gmail.com>
2017-10-09 07:53:04 +0300

    chore(setup-testing): set up the testing environment
      No set up has been done for testing on the lumen bucketlist.
    
      This chore adds mock data as well as sets up the database to be
      used for testing.
    
      [Delivers #151788907]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,14 +1,32 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+use \Laravel\Lumen\Testing\DatabaseMigrations;
+use Symfony\Component\Console\Application;
+
 abstract class TestCase extends Laravel\Lumen\Testing\TestCase
 {
-    /**
+
+     /**
      * Creates the application.
      *
      * @return \Laravel\Lumen\Application
      */
-    public function createApplication()
-    {
-        return require __DIR__.'/../bootstrap/app.php';
-    }
+     public function createApplication()
+     {
+         return require __DIR__.'/../bootstrap/app.php';
+     }
+ 
+     public function setUp()
+     {
+         parent::setUp();
+         $this->refreshApplication();
+         Artisan::call('migrate');
+     }
+ 
+     public function tearDown()
+     {
+         Artisan::call('migrate:reset');
+     }
+ 
 }
